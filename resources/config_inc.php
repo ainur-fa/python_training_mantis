008042732bcdf9e491ae11f6d34b540061d074f1
ainur-fa <ainur_fa@mail.ru>
2016-11-28 21:39:22 +0500

    fail

---
+++ resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Moscow';
+
+$g_crypto_master_salt     = 'v66l7S4IZQZhurpYfnP09TNVjOIJl7M8aosTjxVqWJo=';
+$g_use_captcha 			  =  off;
