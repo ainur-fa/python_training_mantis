344dd63a184b438e948af72aae24dbe1d9bf35b1
ainur-fa <ainur_fa@mail.ru>
2016-11-29 23:45:49 +0500

    telnet

--- fixture/application.py
+++ fixture/application.py
@@ -2,10 +2,11 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.mantis_project import ProjectHelper
+from fixture.james import JamesHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -16,8 +17,10 @@ class Application:
             raise  ValueError("Unrecognized browser %s" % browser)
         #self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
-        self.base_url = base_url
+        self.config=config
+        self.base_url = config['web']['baseUrl']
         self.project = ProjectHelper(self)
+        self.james = JamesHelper(self)
 
     def is_valid(self):
         try:
