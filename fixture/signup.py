288085594d009d73779b482f10c1c57790b53ab2
ainur-fa <ainur_fa@mail.ru>
2016-12-01 00:36:45 +0500

    fail #2

--- fixture/signup.py
+++ fixture/signup.py
@@ -1,4 +1,5 @@
 import re
+import quopri
 
 class SignupHelper:
     def __init__(self, app):
@@ -12,13 +13,11 @@ class SignupHelper:
         wd.find_element_by_css_selector('input[type="submit"]').click()
 
         mail = self.app.mail.get_mail(username, password, "[MantisBT] Account registration")
-        print(mail)
         url = self.extract_confirmation_url(mail)
-        print(url)
         wd.get(url)
         wd.find_element_by_name("password").send_keys(password)
         wd.find_element_by_name("password_confirm").send_keys(password)
         wd.find_element_by_css_selector('input[value="Update User"]').click()
 
     def extract_confirmation_url(self, text):
-        return re.search("http://.*$", text, re.MULTILINE).group(0)
+        return re.search("http://\S*", quopri.decodestring(text).decode()).group(0)
