9c9a1c59e483be3ff5786873afb82aa072a33c12
ainur-fa <ainur_fa@mail.ru>
2016-12-02 00:54:37 +0500

    SOAP

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -5,14 +5,11 @@ def test_add_project(app, db):
     project = Project(name=app.project.random_string("name",10), description=app.project.random_string("description",20))
     #old_listprojects = db.get_project_list()
     app.project.create(project)
-    new_listprojects=db.get_project_list()
+    #new_listprojects=db.get_project_list()
     #assert len(old_listprojects) +1 == len(new_listprojects)
     #old_listprojects.append(project)
     #assert sorted(old_listprojects ,key=Project.id_or_max) == sorted(new_listprojects ,key=Project.id_or_max)
 
     new_list = app.soap.get_project_list()
-
-    print()
-    print(old_list)
-    print(new_list)
-    print(old_list.append(project))
+    old_list +=[project]
+    assert sorted(old_list, key=Project.id_or_max) == sorted(new_list, key=Project.id_or_max)
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -2,13 +2,20 @@ from model.project import Project
 import random
 
 def test_del_project(app, db):
+
     if len(db.get_project_list()) == 0:
         project = Project(name=app.project.random_string("name", 10),
                           description=app.project.random_string("description", 20))
         app.project.create(project)
+
+    old_list = app.soap.get_project_list()
     old_listprojects = db.get_project_list()
     for_delete_project = random.choice(old_listprojects)
     app.project.delete(for_delete_project)
-    new_listprojects = db.get_project_list()
-    old_listprojects.remove(for_delete_project)
-    assert new_listprojects == old_listprojects
+    #new_listprojects = db.get_project_list()
+    #old_listprojects.remove(for_delete_project)
+    #assert new_listprojects == old_listprojects
+    new_list = app.soap.get_project_list()
+    #old_list.remove(for_delete_project)
+    old_list= [elem for elem in old_list if str(elem) != str(for_delete_project)]
+    assert sorted(old_list, key=Project.id_or_max) == sorted(new_list, key=Project.id_or_max)
